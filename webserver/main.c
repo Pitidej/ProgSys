8fdfa75c7c2508eb9c281cce958479cb9d255d63
Renaud Carpentier <renaud.carpentier@gmx.fr>
2016-03-02 15:41:53 +0100

    url_or_404

--- webserver/main.c
+++ webserver/main.c
@@ -14,11 +14,13 @@ int main ()
   int socket_client;
   int pid;
   const char *mess400 = "HTTP/1.1 400 Bad Request\r\nConnection: Close\r\nContent-Length: 17 \r\n\n400 Bad request\r";
+  const char *mess404 = "HTTP/1.1 404 Not found\r\nConnection: Close\r\nContent-Length: 17 \r\n\n400 Bad request\r";
   const char *mess200 = "HTTP/1.1 200 OK\nContent-Length : 23\r\n";
 
   const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur. \n Il est pas interessant mais tant qu'il fonctionne, ca va. \n Sauf que M.Carle ne sera pas satistait. \n En l'occurence, on ne se pose pas assez de question. \n On est con. \n On ne sait pas rechercher dans la doc. \n J'espere que ca fait 10 lignes. \n" ;
   char test[1024];
   char *methode;
+  char *url;
   char *ressources;
   char *spl;
   while ((socket_client = accepte_client(a)) != -1){
@@ -30,17 +32,22 @@ int main ()
       }
       //int i = read(socket_client, message, 1023);
       methode = strtok(message, " ");
-      strtok(NULL, " ");
+      url = strtok(NULL, " ");
       ressources = strtok(NULL, " ");
       spl = strtok(NULL, " ");
       
       //printf("%s\n%s\n%s",methode, ressources, spl);
       
       if ((strcmp(methode,"GET")==0) && ((strstr(ressources,"HTTP/1.0")==0) || (strstr(ressources,"HTTP/1.1")==0)) && (spl == NULL) ){
-	fprintf(fd, "[pawneeeeee] %s \n", mess200);
-	fflush(fd);
-	fprintf(fd,"%s\n",message_bienvenue);
-	fclose(fd);
+	if (strcmp(url,"/")==0){
+	  fprintf(fd, "[pawneeeeee] %s \n", mess200);
+	  fflush(fd);
+	  fprintf(fd,"%s\n",message_bienvenue);
+	  fclose(fd);
+	} else {
+	  fprintf(fd,"%s\n", mess404);
+	  fclose(fd);
+	}
       } else {
 	fprintf(fd,"%s\n",mess400);
 	fflush(fd);
